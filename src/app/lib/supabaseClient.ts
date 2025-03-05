b840d16359b8f8423fbfd159f79518b82bed5809
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-03-05 14:26:19 +0000

    fix: add error handling

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -3,6 +3,14 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseKey) {
+  throw new Error("Missing Supabase key");
+}
+
+if (!supabaseUrl) {
+  throw new Error("Missing Supabase URL");
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 //Sign Up
