5f79ab725b64b71f73f23fa0626cc69da39ef2c0
Monika <mciskowska@gmail.com>
2025-03-06 22:11:25 +0000

    clean up footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -33,17 +33,3 @@ export default function Footer() {
     </footer>
   );
 }
-
-// import styles from "./footer.module.css";
-
-// export default function Footer() {
-//   return (
-//     <footer className={styles.footer}>
-//       <div className={styles.content}>
-//         <p className={styles.credit}>
-//           Created with ❤️ by Kim, Chris, Shanti, Grace, Sam and Monika
-//         </p>
-//       </div>
-//     </footer>
-//   );
-// }
